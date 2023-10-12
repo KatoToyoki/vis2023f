3e5a21a964f1464fc279d3384f53535d5571288c
tyk <110590015@ntut.org.tw>
2023-10-12 16:06:17 +0800

    [progress] apple header done

--- hw01/src-easy/main.js
+++ hw01/src-easy/main.js
@@ -1,5 +1,29 @@
-function CSVtoTable() {
-  let reader = new FileReader();
-  let table = document.getElementById("score_table");
-  reader.readAsText("../score.csv");
+function AppleTable() {
+  let table = document.getElementById("apple");
+  table.appendChild(AppleIconRow());
+}
+
+function AppleIconRow() {
+  let row = document.createElement("tr");
+
+  for (let i = 10; i >= 0; i--) {
+    let col = document.createElement("td");
+
+    col.textContent = i;
+
+    col.appendChild(document.createElement("hr"));
+
+    let img = document.createElement("img");
+    img.src = `../images/${i}.svg`;
+    img.width = "80";
+    col.appendChild(img);
+
+    row.appendChild(col);
+  }
+
+  return row;
+}
+
+function WebOnLoad() {
+  AppleTable();
 }
